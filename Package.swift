739560c8b126249882de6d1e0caa6d96e329f63b
Oleh Sehelyn <savar07@gmail.com>
2025-07-25 15:46:17 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "Sliders-SwiftUI", url: "https://github.com/olegsehelin/Sliders-SwiftUI.git", .branch("master")),
         .package(name: "SwiftUI-Shapes", url: "https://github.com/olegsehelin/SwiftUI-Shapes.git", .branch("master")),
-        .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.3")
+        .package(url: "https://github.com/olegsehelin/CGExtender.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
